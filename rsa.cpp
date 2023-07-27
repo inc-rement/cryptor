74e7354cddc2924de38b51d4a1fedbf4cf1597c6
Denis Vergun <blu.eagle@mail.ru>
2023-07-28 04:14:28 +0500

    h

--- rsa.cpp
+++ rsa.cpp
@@ -6,9 +6,9 @@
 
 
 //naibolsiy obshiy delitel
-int gcd(int a, int h)
+int gcd(long int a, long int h)
 {
-    int temp;
+    long int temp;
     while (1)
     {
         temp = a%h;
@@ -21,10 +21,11 @@ int gcd(int a, int h)
 
 
 int main(){
-int p=3,q=7;//prostie chisla
+int p=3,q=11;//prostie chisla
 int n=p*q;//module
+std::cout <<"n "<< n << std::endl;
 int phi=(p-1)*(q-1);//func Eilera
-
+std::cout << "phi " << phi <<std::endl;
 int e=2;//min prostoe
 while(e<phi){
 //nahojdeniye e открытой экспоненты
@@ -34,24 +35,37 @@ if (gcd(e, phi)==1)
             e++;
 
 }
+//e=7;
+std::cout<<"e "<<e<<std::endl;
 int k=2;//mnogitel
 int d = (1 + (k*phi))/e; //вычислить секретную экспоненту
-
-int msg;
-std::cout<<"input number: "<<std::endl;
-std::cin>>msg;
+//d=3;
+std::cout<<"d "<<d<<std::endl;
+int msg=3;
+//std::cout<<"input: "<<std::endl;
+//
+//std::cin>>msg;
 
 
 std::cout<<"msg: "<<msg<<std::endl;
+//int N = 5;
+//int x = (int)character - 48;
 
-int c=(int)(pow(msg, e))%n;
+//std::cout<<N<<std::endl;
+
+
+//std::cout<<static_cast<char>(N)<<std::endl;
+
+
+
+int c=modf(int(pow(msg,e)),n);
 
 
 std::cout<<"encrypted: "<<c<<std::endl;
 
 
 
-int m = (int)(pow(c, d))%n;
+int m = int(pow(c,d))%n;
 
 
 std::cout<<"decrypted: "<<m<<std::endl;
