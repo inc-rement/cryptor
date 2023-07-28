b46f8df4d69780364d9170b69a92c622f849a4d3
Denis Vergun <blu.eagle@mail.ru>
2023-07-29 00:41:12 +0500

    cd XOR;makewq

--- XOR/rsa.cpp
+++ XOR/rsa.cpp
@@ -3,6 +3,13 @@
 #include <cfenv>
 #include <cmath>
 #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int public_key;
+int private_key;
+int n;
 
 
 //naibolsiy obshiy delitel
@@ -18,11 +25,55 @@ int gcd(long int a, long int h)
         h = temp;
     }
 }
+void setkeys()
+{
+        int prime1 = 11;//pickrandomprime(); // first prime number
+        int prime2 = 17;//pickrandomprime(); // second prime number
+        // to check the prime numbers selected
+        // cout<<prime1<<" "<<prime2<<endl;
+        n = prime1 * prime2;
+        int fi = (prime1 - 1) * (prime2 - 1);
+        int e = 2;
+        while (1) {
+                if (__gcd(e, fi) == 1)
+                        break;
+                e++;
+        } // d = (k*<CE><A6>(n) + 1) / e for some integer k
+        public_key = e;
+        int d = 2;
+        while (1) {
+                if ((d * e) % fi == 1)
+                        break;
+                d++;
+        }
+        private_key = d;
+}
 
+long long int encrypt(double message)
+{
+        int e = public_key;
+        long long int encrpyted_text = 1;
+        while (e--) {
+                encrpyted_text *= message;
+                encrpyted_text %= n;
+        }
+        return encrpyted_text;
+}
+// to decrypt the given number
+long long int decrypt(int encrpyted_text)
+{
+        int d = private_key;
+        long long int decrypted = 1;
+        while (d--) {
+                decrypted *= encrpyted_text;
+                decrypted %= n;
+        }
+        return decrypted;
+}
 
 int main(){
-int p=3,q=7;//prostie chisla
-int n=p*q;//module
+int p=11,q=17;//prostie chisla
+n=p*q;//module
 std::cout <<"n "<< n << std::endl;
 int phi=(p-1)*(q-1);//func Eilera
 std::cout << "phi " << phi <<std::endl;
@@ -41,7 +92,11 @@ int k=2;//mnogitel
 int d = (1 + (k*phi))/e; //вычислить секретную экспоненту
 //d=3;
 std::cout<<"d "<<d<<std::endl;
-int msg=4;
+setkeys();
+//int public_key=e;
+//int private_key=d;
+
+int msg=3;
 //std::cout<<"input: "<<std::endl;
 //
 //std::cin>>msg;
@@ -58,18 +113,18 @@ std::cout<<"msg: "<<msg<<std::endl;
 
 
 
-long long int c=pow(msg,e);
-c=c%n;
+long long int c=encrypt(msg);
+//c=c%n;
 
 
-std::cout<<"encrypted: "<<c<<std::endl;
+std::cout<<"encrypted: "<<encrypt(msg)<<std::endl;
 
 
 
-long long int m = pow(c,d);
+long long int m = decrypt(c);
 
-m=m%n;
-std::cout<<"decrypted: "<< m <<std::endl;
+//m=m%n;
+std::cout<<"decrypted: "<< decrypt(encrypt(msg)) <<std::endl;
 
 return 0;
 
