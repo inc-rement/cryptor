af2662327ae058e96fb4d0f7b81852cd5fec15eb
Denis Vergun <blu.eagle@mail.ru>
2023-08-11 20:38:49 +0500

    test

---
+++ decryptor.h
@@ -0,0 +1,8 @@
+int decrypt(){
+long long int decrypted = 1;
+       // while (d--) {
+        //        decrypted *= encrpyted_text;
+        //        decrypted %= n;
+        //}
+
+}
---
+++ encryptor.h
@@ -0,0 +1,9 @@
+
+int encrypt(int message){
+long long int encrpyted_text = 1;
+       // while (e--) {
+        //        encrpyted_text *= x;
+         //       encrpyted_text %= n;
+       // }
+	return message;
+}
