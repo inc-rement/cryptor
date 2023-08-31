38b0423a3b9caed2c87b3fe3a7f3e6e3c0c8dee9
Denis Vergun <blu.eagle@mail.ru>
2023-08-31 15:22:59 +0500

    make;//input any number, press inter

--- visio.cpp
+++ visio.cpp
@@ -43,7 +43,7 @@ double m=0;
 double x1;// = 1;
 bool f;
    while (1) {
-  //         XClearWindow(d, w);
+     //      XClearWindow(d, w);
 
 // 	if(f)f=false;else f = true;     
 //XNextEvent(d, &e);
@@ -70,17 +70,17 @@ bool f;
 	 for(double x =-5.45;x<11.64;x+=0.01){
 // double m=0;
 //if(x1<0)m=(pow(x1,2))-2;else if(x1>0)m=pow(x1,2)+2; else m=pow(x1,2);
-t.seta(t.getX());
-mpf_pow_ui(c.get_mpf_t(), t.geta().get_mpf_t(), 2);
-mpf_sqrt(g.get_mpf_t(),c.get_mpf_t());
+///t.seta(t.getX());
+///mpf_pow_ui(c.get_mpf_t(), t.geta().get_mpf_t(), 2);
+///mpf_sqrt(g.get_mpf_t(),c.get_mpf_t());
 
-m=g.get_d()-5+3;
+///m=g.get_d()-5+3;
 //m=sqrt(pow(t.getX(),2))-5+3;
-m=m*m;
+///m=m*m;
 
-t.setY(sqrt((pow(t.getX(),3))+ecc.getB()*t.getX()));
-y1=t.getY();
-x1=t.getX();
+///t.setY(sqrt((pow(t.getX(),3))+ecc.getB()*t.getX()));
+///y1=t.getY();
+///x1=t.getX();
 //t.setY((double)y1);
 //if(x1>0)m=sqrt((pow(x1,2)))-2-5;
 //double y1 = (pow(x1,3));//pow(x1,3)-25*x1));
@@ -90,34 +90,39 @@ x1=t.getX();
 //if((m*(x-x1)+y1)>0)XDrawPoint(d, w, DefaultGC(d, s), (x*10)+500, 500-(m*(x-x1)+y1)*10);
 //if((m*(x-x1)+y1)<0)XDrawPoint(d, w, DefaultGC(d, s), 500-abs(x*10), 500+(m*(abs(x)-x1)+y1)*10);
 
-Line tangent(1,1);
-XDrawPoint(d, w, DefaultGC(d, s), (x*10)+500, 500-(m*(x-x1)+y1)*10);
+///Line tangent(1,1);
+///XDrawPoint(d, w, DefaultGC(d, s), (x*10)+500, 500-(m*(x-x1)+y1)*10);
 
-XDrawArc(d, w, DefaultGC(d, s), t.getX()*10+497, 497-t.getY()*10, 5, 5, 0, 360 * 64);
-x3=((pow(m,2))-2*x1);
-y3=(m*(x1-x3)-y1);
+///XDrawArc(d, w, DefaultGC(d, s), t.getX()*10+497, 497-t.getY()*10, 5, 5, 0, 360 * 64);
+///x3=((pow(m,2))-2*x1);
+///y3=(m*(x1-x3)-y1);
 
-p.setX(x3);
-p.setY(y3);
-m1.seta(123456789.987654321);
-m1.setb(987654321.123456789);
+///p.setX(x3);
+///p.setY(y3);
+///m1.seta(123456789.987654321);
+///m1.setb(987654321.123456789);
 
 //if(x1>0)x3=((pow(m,2))+x1-x1);
 
-XDrawArc(d, w, DefaultGC(d, s), 497+x3*10, 497-y3*10, 5, 5, 0, 360 * 64);
+///XDrawArc(d, w, DefaultGC(d, s), 497+x3*10, 497-y3*10, 5, 5, 0, 360 * 64);
 
-XDrawPoint(d, w ,DefaultGC(d, s), (x*10)+500, 500-(sqrt(pow(x,3)-25*x))*10);
-XDrawPoint(d, w ,DefaultGC(d, s), (x*10)+500, (sqrt(pow(x,3)-25*x)*10)+500);
-	 }
+///XDrawPoint(d, w ,DefaultGC(d, s), (x*10)+500, 500-(sqrt(pow(x,3)-25*x))*10);
+///XDrawPoint(d, w ,DefaultGC(d, s), (x*10)+500, (sqrt(pow(x,3)-25*x)*10)+500);
+XDrawPoint(d, w ,DefaultGC(d, s), (x*10)+500, 500-(sqrt(pow(x,3)*1/3+pow(x,2)*1/2+1/6*x))*10);
+XDrawPoint(d, w ,DefaultGC(d, s), (x*10)+500, (sqrt(pow(x,3)*1/3+pow(x,2)*1/2+1/6*x)*10)+500);
+
+
+
+ }
 
 
 if(f){
 std::cout<<"---------"<<std::endl;
-std::cout<<std::round(p.getY())<<std::endl;
-std::cout<<std::round((sqrt(abs(pow(x3,3))+ecc.getB()*p.getX())))<<std::endl;
-std::cout << std::setprecision(20);
-std::cout<<m1.geta().get_d()<<std::endl;
-std::cout<<m1.getb().get_d()<<std::endl;
+//std::cout<<std::round(p.getY())<<std::endl;
+//std::cout<<std::round((sqrt(abs(pow(x3,3))+ecc.getB()*p.getX())))<<std::endl;
+//std::cout << std::setprecision(20);
+//std::cout<<m1.geta().get_d()<<std::endl;
+//std::cout<<m1.getb().get_d()<<std::endl;
 
 }
 
