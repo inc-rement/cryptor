1bcc948e2d994f70411f06ae05820c2759cabb04
Denis Vergun <blu.eagle@mail.ru>
2023-09-15 15:56:36 +0500

    use

--- encryptor.h
+++
@@ -1,9 +0,0 @@
-
-int encrypt(int message){
-long long int encrpyted_text = 1;
-       // while (e--) {
-        //        encrpyted_text *= x;
-         //       encrpyted_text %= n;
-       // }
-	return message;
-}
