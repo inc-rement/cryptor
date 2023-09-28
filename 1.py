aec0858f94d4c6033fc01a065457d8127a79e798
Denis Vergun <blu.eagle@mail.ru>
2023-09-28 12:28:12 +0500

    python 1.py  =>  4.0      [14,-53] см README для проверки

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ import time
 
 
 def g_l (P, Q):
-    print(P, Q)
+    #print(P, Q)
     if P == '0':
         return Q
     if Q == '0':
@@ -11,9 +11,11 @@ def g_l (P, Q):
     if (P[0] == Q[0]) and (P[1] == -Q[1]):
         return '0'
     if (P[0] == Q[0]) and (P[1] == Q[1]):
-        m = (3*P[0]**2+a)/2/P[1]
+        m = (3*P[0]**2+5)/(2*P[1])
+        print(m) 
     else:
         m = (Q[1]-P[1])/(Q[0]-P[0])
+        print(m)
     x3 = m**2-P[0] -Q[0]
     return [x3 ,m*(P[0] - x3)-P[1]]
 
@@ -35,24 +37,24 @@ def mult_2 (n,P):
 
 #E = EllipticCurve (GF (1000000007) ,[ -6 ,5])
 #P = E ([2 ,1])
-a = math.fmod( -6 ,5)
-b = math.fmod(5 ,5)
-P=[2,1]
+#a = math.fmod( -6 ,5)
+#b = math.fmod(5 ,5)
+#P=[2,1]
 
-start = time.time()
-(print(easy_mult(10**6,P)))
-end = time.time()
-print("The time of execution of above program is :",
-        (end-start) * 10**3, "ms")
-
-
-start = time.time()
-(print(mult_2(10**6, P)))
-end = time.time()
-print("The time of execution of above program is :",
-        (end-start) * 10**3, "ms")
+#start = time.time()
+#(print(easy_mult(10**6,P)))
+#end = time.time()
+#print("The time of execution of above program is :",
+#        (end-start) * 10**3, "ms")
 
 
-P= [5 ,17]
-a= math.fmod(1, 10403)
-print(mult_2(math.factorial(7) , P ))
+#start = time.time()
+#(print(mult_2(10**6, P)))
+#end = time.time()
+#print("The time of execution of above program is :",
+#        (end-start) * 10**3, "ms")
+
+
+P= [1 ,1]
+#a= math.fmod(1, 10403)
+print(easy_mult(2 , P ))
