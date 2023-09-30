ac309c02a602063325046e9db09d22c12c81b7fa
Denis Vergun <blu.eagle@mail.ru>
2023-09-30 13:24:11 +0500

    python product.py slope output

---
+++ product.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import math
+import sys 
+
+def check(L):
+    print((L[1]**2)%n==(L[0]**3+10*L[0]-2)%n)
+
+
+n=4453
+P=[1,3]
+(check(P))
+#print((13*3711)%n)
+y=(3*P[0]**2+10)%n
+x=(2*P[1])%n
+
+for i in range(5000):
+    if((i*6)%4453)==1:
+    #if((13/6)%4453)==i%4453:
+        print((y*i)%n)
