42c72ed66eb8ca3d3a1b130ffb3823f9f5f7313c
Denis Vergun <blu.eagle@mail.ru>
2023-10-01 13:31:33 +0500

    ./product.py

--- product.py
+++ product.py
@@ -8,12 +8,17 @@ def check(L):
 
 n=4453
 P=[1,3]
-(check(P))
+#(check(P))
 #print((13*3711)%n)
 y=(3*P[0]**2+10)%n
 x=(2*P[1])%n
-
+m=0
 for i in range(5000):
     if((i*6)%4453)==1:
     #if((13/6)%4453)==i%4453:
-        print((y*i)%n)
+        m=((y*i)%n)
+        print('m = ', m)
+
+x1=(m**2-2)%4453
+y1=(-m*(x1-1)-3)%4453
+print('2P = ', x1, y1)
