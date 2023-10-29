af92b49128cfea0a44f65515f04d26efeb754d12
Denis Vergun <blu.eagle@mail.ru>
2023-10-29 21:04:35 +0500

    curve.p,  curve.n,  e, d in factor4.py

--- factor4.py
+++ factor4.py
@@ -7,24 +7,18 @@ from curve import Curve
 import numpy as np
 from curve import bi, ib
 
-
 curve=Curve()
-#print(curve.n/curve.p)
-#curve.g=([48439561293906451759052585252797914202762949526041747995844080717082404635286, 36134250956749795798585127919587881956611106672985015071877198253568414405109])
 
 curve.p=9743
 curve.n=94984507
-#print(curve.g)
+e=251
+d=189251
 
 class Cryptor:
     def __init__(self):
         self.message=0    
         self.l=0
         self.h=0
-        #inputs = list(sys.stdin)  # Get all of stdin
-        #inputs = inputs[0]  # Get only the first line
-        #inputs = inputs.split()  #  p
-        #self.X=0 #int(inputs[0])
         self.x=0
         self.y=0
 
@@ -41,8 +35,8 @@ class Cryptor:
         curve.g=(self.x,int(math.sqrt(self.y)))
         #print(curve.valid(curve.g))
         #print(curve.g)
-        eM=curve.mul(curve.g, 251)
-        dM=curve.mul(eM, 189251)
+        eM=curve.mul(curve.g, e)
+        dM=curve.mul(eM, d)
         #print(dM)
         #print(curve.valid(dM))
         print("message: ",math.floor((dM[0]/(28*10))))
