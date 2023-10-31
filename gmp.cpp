ed377a33227ca75c5f3d0f260a07e64fa44ad94f
Denis Vergun <blu.eagle@mail.ru>
2023-10-31 12:31:47 +0500

    make (a.out hold)

--- gmp.cpp
+++ gmp.cpp
@@ -7,9 +7,9 @@ int main(void)
 
    mpz_class cc, t;
    t=23;
-   mpz_pow_ui(cc.get_mpz_t(), t.get_mpz_t(), 46);
+   mpz_pow_ui(cc.get_mpz_t(), t.get_mpz_t(), 77777776);
    //cc=t^46;
-   std::cout<<"23^46: "<<cc<<std::endl;
+   std::cout<<cc<<std::endl;
 
 
    return EXIT_SUCCESS;
